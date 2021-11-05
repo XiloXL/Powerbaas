d35645e5a9c8160a0a580c9a0e2b6ea97a7df332
gerardoudenampsen <goudenampsen@gmail.com>
2021-11-05 08:34:08 +0100

    fixed typo improved comments

--- src/MeterReading.h
+++ src/MeterReading.h
@@ -8,15 +8,15 @@ struct MeterReading {
   // this is the amount of power you are currently using, negative if you return to the net, positive if you use from the net
   int32_t powerUsage;
 
-  // the amount of power you are using from the net, low and high rates (some smart meters use 2 rates)
+  // the amount of power you have used from the net, low and high rates (some smart meters use 2 rates)
   uint32_t powerDeliverHigh;
   uint32_t powerDeliverLow;
 
-  // the amount of power you are returning to the net, low and high rates (some smart meters use 2 rates)
+  // the amount of power you have returned to the net, low and high rates (some smart meters use 2 rates)
   uint32_t powerReturnHigh;
   uint32_t powerReturnLow;
 
-  // the amount of gas you are using from the net
+  // the amount of gas you have used from the net
   uint32_t gas;
 };
 
